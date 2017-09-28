aa71ae97bebf3293ab9f1b8b7d4c415eb26abec5
kitayo <kitayo0821@gmail.com>
2017-09-28 11:18:28 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+
+var express = require("express");
+
+var app = express();
+
+app.get("/hello", function(req, res){
+  res.send("Hello!");
+});
+
+app.all("*", function(req, res){
+    res.sendStatus(404);
+});
+
+app.listen(3000, function(){
+    console.log("Running on port 3000");
+});
